4569e5618c82b41036cff2cf6efbbd08a2d6ccab
Gabriel Herve <81375156+gabrielOtaku@users.noreply.github.com>
2026-04-21 11:37:34 -0400

    Projet de jeux construction de la base

---
+++ BreakoutGameCarlosGabriel/BasePourObjets.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BreakoutGameCarlosGabriel
+{
+    public abstract class BasePourObjets
+    {
+        // Attributs communs (protégés pour être accessibles par les enfants)
+        protected float positionX;
+        protected float positionY;
+        protected float largeur;
+        protected float hauteur;
+
+        // Propriétés publiques
+        public float PositionX { get => positionX; set => positionX = value; }
+        public float PositionY { get => positionY; set => positionY = value; }
+        public float Largeur { get => largeur; set => largeur = value; }
+        public float Hauteur { get => hauteur; set => hauteur = value; }
+
+        // Méthodes communes abstraites
+        public abstract void MettreAJour();
+
+        // La méthode Dessiner nécessitera probablement un objet graphique (ex: Graphics g) selon votre cadriciel
+        public abstract void Dessiner();
+    }
+}

d66bd82389c366206cfcbef49d1b62cefb8522bf
Gabriel Herve <81375156+gabrielOtaku@users.noreply.github.com>
2026-04-21 11:49:28 -0400

    Creation de tout les template du jeu avec l'ensemble des fichiers, Faire les TODO

---
+++ BreakoutGameCarlosGabriel/FichierWAV.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace BreakoutGameCarlosGabriel
+{
+    public class FichierWAV
+    {
+        private string nomFichier;
+        private int nbrCanaux;
+        private int frequence;
+        private int nbrBits;
+        private int qteDonneesSonores;
+        private byte[] donneesSonores;
+
+        public FichierWAV(string nomFichier)
+        {
+            this.nomFichier = nomFichier;
+            // TODO : Appeler ChargerFichier avec un flux (Stream)
+        }
+
+        private void ChargerFichier(Stream stream)
+        {
+            // TODO : Implémenter la lecture du fichier WAV
+        }
+
+        public byte[] GetDonneesSonores() { return donneesSonores; }
+        public int GetQteDonneesSonores() { return qteDonneesSonores; }
+        public int GetFrequence() { return frequence; }
+
+        // Note: Le type ALFormat dépend de la librairie audio que vous utilisez (comme OpenTK)
+        // public ALFormat GetFormatSonAL() { ... }
+    }
+}

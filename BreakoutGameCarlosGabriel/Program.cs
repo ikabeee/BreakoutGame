a0bedcb572a55eb694ed05d966e1ab203367a217
Carlos Gonzalez <Carlos Gonzalez@DESKTOP-1OK40MO>
2026-04-22 09:09:30 -0400

    Réorganisation des sources et init OpenTK dans Program.cs
    Les fichiers sources ont été déplacés dans des sous-dossiers dédiés et le .csproj a été mis à jour en conséquence. Program.cs utilise désormais OpenTK pour initialiser la fenêtre du jeu avec gestion du moniteur. La classe Raquette est maintenant abstraite et son contenu a été retiré.

--- BreakoutGameCarlosGabriel/Program.cs
+++ BreakoutGameCarlosGabriel/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OpenTK;
+using OpenTK.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,17 @@ namespace BreakoutGameCarlosGabriel
     {
         static void Main(string[] args)
         {
+            int largeurFenetre = 600;
+            int hauteurFenetre = 300;
+            string titreFenetre = "Breakout Game - Carlos Gabriel";
+
+            DisplayDevice moniteur = DisplayDevice.Default;
+            if (DisplayDevice.Default == DisplayDevice.GetDisplay(DisplayIndex.Second))
+            {
+                moniteur = DisplayDevice.GetDisplay(DisplayIndex.First);
+            }
+            GameWindow window = new GameWindow(largeurFenetre, hauteurFenetre, GraphicsMode.Default, titreFenetre, GameWindowFlags.Default, moniteur);
+            GestionJeu fenetrePrincipale = new GestionJeu(window);
         }
     }
 }
--- BreakoutGameCarlosGabriel/Raquette/Raquette.cs
+++ BreakoutGameCarlosGabriel/Raquette/Raquette.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace BreakoutGameCarlosGabriel
 {
-    internal class Raquette
+    abstract class Raquette
     {
+
     }
 }
